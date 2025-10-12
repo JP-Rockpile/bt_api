299aec0fd47a3c03f66c282b2a113b19c706f14d
Carter <cjwilley23@gmail.com>
2025-10-11 18:19:51 -0600

    updated e2e tests

--- src/modules/bets/bets.module.ts
+++ src/modules/bets/bets.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BetsService } from './bets.service';
 import { BetsController } from './bets.controller';
+import { DeepLinkService } from './services/deep-link.service';
 
 @Module({
   controllers: [BetsController],
-  providers: [BetsService],
+  providers: [BetsService, DeepLinkService],
   exports: [BetsService],
 })
 export class BetsModule {}
